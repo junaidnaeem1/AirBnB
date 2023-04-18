443f40eba714b322d5b6e107fad76f1681247798
junaidnaeem1 <junnaeem2@gmail.com>
2023-04-17 22:13:57 -0400

    added-seeder

--- backend/db/seeders/20230413230948-demo-user.js
+++ backend/db/seeders/20230413230948-demo-user.js
@@ -11,16 +11,22 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
+        firstName: 'Junaid',
+        lastName: 'NAEEM',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Junaid',
+        lastName: 'NAEEM',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Junaid',
+        lastName: 'NAEEM',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
